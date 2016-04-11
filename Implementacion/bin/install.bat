f004c7a6d3bce6491c5efb03dbed0c53cf235594
Pedro Martin del Campo González <pedro.martin@uabc.edu.mx>
2016-04-11 12:33:44 -0800

    Program AB AIML Analyzer integrated

---
+++ Implementacion/bin/install.bat
@@ -0,0 +1,14 @@
+cd ..
+mvn install:install-file -Dfile=lib/commons-codec-1.6.jar -DgroupId=commons-codec -DartifactId=commons-codec -Dpackaging=jar -Dversion=1.6 -o
+mvn install:install-file -Dfile=lib/commons-logging-1.1.1.jar -DgroupId=commons-logging -DartifactId=commons-logging -Dpackaging=jar -Dversion=1.1.1 -o
+mvn install:install-file -Dfile=lib/fluent-hc-4.2.1.jar -DgroupId=fluent-hc -DartifactId=fluent-hc -Dpackaging=jar -Dversion=4.2.1 -o
+mvn install:install-file -Dfile=lib/hamcrest-core-1.3.jar -DgroupId=hamcrest-core -DartifactId=hamcrest-core -Dpackaging=jar -Dversion=1.3 -o
+mvn install:install-file -Dfile=lib/httpclient-4.2.1.jar -DgroupId=httpclient -DartifactId=httpclient -Dpackaging=jar -Dversion=4.2.1 -o
+mvn install:install-file -Dfile=lib/httpclient-cache-4.2.1.jar -DgroupId=httpclient-cache -DartifactId=httpclient-cache -Dpackaging=jar -Dversion=4.2.1 -o
+mvn install:install-file -Dfile=lib/httpcore-4.2.1.jar -DgroupId=httpcore -DartifactId=httpcore -Dpackaging=jar -Dversion=4.2.1 -o
+mvn install:install-file -Dfile=lib/httpmime-4.2.1.jar -DgroupId=httpmime -DartifactId=httpmime -Dpackaging=jar -Dversion=4.2.1 -o
+mvn install:install-file -Dfile=lib/joda-time-2.9.3.jar -DgroupId=joda-time -DartifactId=joda-time -Dpackaging=jar -Dversion=2.9.3 -o
+mvn install:install-file -Dfile=lib/json-20090211.jar -DgroupId=json -DartifactId=json -Dpackaging=jar -Dversion=20090211 -o
+mvn install:install-file -Dfile=lib/junit-4.12.jar -DgroupId=junit -DartifactId=junit -Dpackaging=jar -Dversion=4.12 -o
+mvn install:install-file -Dfile=lib/program-ab-1.0.jar -DgroupId=program-ab -DartifactId=program-ab -Dpackaging=jar -Dversion=1.0 -o
+mvn install:install-file -Dfile=lib/sanmoku-0.5.jar -DgroupId=sanmoku -DartifactId=sanmoku -Dpackaging=jar -Dversion=0.5 -o
